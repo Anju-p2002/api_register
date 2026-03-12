5e179c9090ba4fa0f578fa1687f74e41e6eeb51f
Anju-p2002 <anjuprasannan443@gmail.com>
2026-03-12 13:14:25 +0530

    def

---
+++ home/views.py
@@ -0,0 +1,29 @@
+from django.shortcuts import render
+from .serializers import UserRegister
+from rest_framework.views import APIView
+from rest_framework.authtoken.models import Token
+from rest_framework.response import Response
+
+# Create your views here.
+
+class Register(APIView):
+
+    def post(self,request, formate=None):
+
+        serializer = UserRegister(data=request.data)
+        data = {}
+
+        if serializer.is_valid():
+            account = serializer.save()
+
+            data['response'] = 'registered'
+            data['username'] = account.username
+            data['email'] = account.email
+
+            token, created = Token.objects.get_or_create(user=account)
+            data['token']= token.key
+
+        else:
+            data = serializer.errors
+            
+        return Response(data)
